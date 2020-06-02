22df9e3c40d9b34755ecc7d4aaad30a59113e910
Dragos2099 <dragos.tudosoiu@student.upt.ro>
2020-06-02 21:05:09 +0300

    PEM-31 Implementare Home-Administrator

--- src/main/java/Log_in/controllers/Login.java
+++ src/main/java/Log_in/controllers/Login.java
@@ -2,10 +2,15 @@ package Log_in.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
 import java.io.IOException;
 import java.util.Objects;
 
@@ -37,7 +42,15 @@ public class Login {
         }
 
         if(Objects.equals(username,"administrator") && Objects.equals(password,"administrator")){
-            loginMessage.setText("Administrator");
+            try {
+                Stage stage = (Stage) loginMessage.getScene().getWindow();
+                stage.setTitle("Administrator");
+                Parent AdmHome = FXMLLoader.load(getClass().getResource("/HomeAdministrator.fxml"));
+                Scene scene = new Scene(AdmHome, 700, 500);
+                stage.setScene(scene);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
         else
         if(Objects.equals(username,"manager") && Objects.equals(password,"manager")){
