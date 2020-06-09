df5aa3a7b175c22192e78415c87a658f3747c965
Dragos2099 <dragos.tudosoiu@student.upt.ro>
2020-06-09 19:22:54 +0300

    PEM-106 Logarea se face pe baza fisierului json

--- src/main/java/Log_in/controllers/Login.java
+++ src/main/java/Log_in/controllers/Login.java
@@ -1,5 +1,11 @@
 package Log_in.controllers;
 
+import Register.model.User;
+import Register.services.Criptare;
+import Register.services.FileSystemService;
+import Register.services.UserService;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,12 +17,17 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import org.apache.commons.io.FileUtils;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 import java.util.Objects;
 
 public class Login {
-
+    private static List<User> users;
+    private static final Path USERS_PATH = FileSystemService.getPathToFile("utilizatori", "users.json");
 
     @FXML
     public Text loginMessage;
@@ -28,7 +39,7 @@ public class Login {
     public Button btn_msg;
 
 
-    public void pressButton(ActionEvent actionEvent) {
+    public void pressButton(ActionEvent actionEvent) throws IOException {
         String username = usernameField.getText();
         String password = passwordField.getText();
 
@@ -39,43 +50,58 @@ public class Login {
         if (password == null || password.isEmpty()) {
             loginMessage.setText("Introduceti parola !");
             return;
-        } else
-        if(Objects.equals(username,"administrator") && Objects.equals(password,"administrator")){
-            try {
-                Stage stage = (Stage) loginMessage.getScene().getWindow();
-                stage.setTitle("Administrator");
-                Parent AdmHome = FXMLLoader.load(getClass().getResource("/HomeAdministrator.fxml"));
-                Scene scene = new Scene(AdmHome, 700, 500);
-                stage.setScene(scene);
-            } catch (IOException e) {
-                e.printStackTrace();
+        }
+
+        if (!Files.exists(USERS_PATH)) {
+            FileUtils.copyURLToFile(UserService.class.getClassLoader().getResource("users.json"), USERS_PATH.toFile());
+        }
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        users = objectMapper.readValue(USERS_PATH.toFile(), new TypeReference<List<User>>() {
+        });
+
+        for (User user : users) {
+            String pws= Criptare.decrypt(user.getPassword(),user.getUsername());
+            if (Objects.equals(username, user.getUsername()) && Objects.equals(password,pws)) {
+                if(Objects.equals(user.getRole(),"Administrator")) {
+                    try {
+                        Stage stage = (Stage) loginMessage.getScene().getWindow();
+                        stage.setTitle("Administrator");
+                        Parent AdmHome = FXMLLoader.load(getClass().getResource("/HomeAdministrator.fxml"));
+                        Scene scene = new Scene(AdmHome, 700, 500);
+                        stage.setScene(scene);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                if(Objects.equals(user.getRole(),"Manager")) {
+                    try {
+                        Stage stage = (Stage) loginMessage.getScene().getWindow();
+                        stage.setTitle("Manager");
+                        Parent AdmHome = FXMLLoader.load(getClass().getResource("/HomeManager.fxml"));
+                        Scene scene = new Scene(AdmHome, 700, 500);
+                        stage.setScene(scene);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if(Objects.equals(user.getRole(),"Furnizor")) {
+                    try {
+                        Stage stage = (Stage) loginMessage.getScene().getWindow();
+                        stage.setTitle("Furnizor");
+                        Parent AdmHome = FXMLLoader.load(getClass().getResource("/HomeFurnizor.fxml"));
+                        Scene scene = new Scene(AdmHome, 700, 500);
+                        stage.setScene(scene);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
         }
-        else
-        if(Objects.equals(username,"manager") && Objects.equals(password,"manager")){
-            try {
-                Stage stage = (Stage) loginMessage.getScene().getWindow();
-                stage.setTitle("Manager");
-                Parent AdmHome = FXMLLoader.load(getClass().getResource("/HomeManager.fxml"));
-                Scene scene = new Scene(AdmHome, 700, 500);
-                stage.setScene(scene);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        else
-        if(Objects.equals(username,"furnizor") && Objects.equals(password,"furnizor")){
-            try {
-                Stage stage = (Stage) loginMessage.getScene().getWindow();
-                stage.setTitle("Furnizor");
-                Parent AdmHome = FXMLLoader.load(getClass().getResource("/HomeFurnizor.fxml"));
-                Scene scene = new Scene(AdmHome, 700, 500);
-                stage.setScene(scene);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        else loginMessage.setText("Date invalide !!");
+
+        loginMessage.setText("Contul nu exista !");
 
 
     }
