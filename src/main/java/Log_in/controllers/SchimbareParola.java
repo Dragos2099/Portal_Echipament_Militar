873bc1df5b992dce732d6a26e864b0e0dfb4c94f
Dragos2099 <dragos.tudosoiu@student.upt.ro>
2020-06-11 20:32:25 +0300

    PEM-40 Mesaj de eroare in cazul datelor incorecte

--- src/main/java/Log_in/controllers/SchimbareParola.java
+++ src/main/java/Log_in/controllers/SchimbareParola.java
@@ -1,4 +1,77 @@
 package Log_in.controllers;
 
+import Register.exceptions.CouldNotWriteUserException;
+import Register.model.User;
+import Register.services.Criptare;
+import Register.services.FileSystemService;
+import Register.services.UserService;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import org.apache.commons.io.FileUtils;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Objects;
+
 public class SchimbareParola {
+    private static List<User> users;
+    private static final Path USERS_PATH= FileSystemService.getPathToFile("utilizatori","users.json");
+    @FXML
+    public Text ChangeMessage;
+    @FXML
+    public TextField UsernameField;
+    @FXML
+    public javafx.scene.control.PasswordField OldPasswordField;
+    @FXML
+    public javafx.scene.control.PasswordField NewPasswordField;
+
+    public void scmb_parola(ActionEvent actionEvent) throws IOException {
+        String username = UsernameField.getText();
+        String oldpassword = OldPasswordField.getText();
+        String newpassword = NewPasswordField.getText();
+        if (username == null || username.isEmpty()) {
+            ChangeMessage.setText("E-mail invalid");
+            return;
+        } if (oldpassword == null || oldpassword.isEmpty()) {
+            ChangeMessage.setText("Parola introdusa este gresita");
+            return;
+        }
+        if (!Files.exists(USERS_PATH)) {
+            FileUtils.copyURLToFile(UserService.class.getClassLoader().getResource("users.json"), USERS_PATH.toFile());
+        }
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        users = objectMapper.readValue(USERS_PATH.toFile(), new TypeReference<List<User>>() {
+        });
+
+        for (User user : users) {
+            String pws=Criptare.decrypt(user.getPassword(),user.getUsername());
+            if(Objects.equals(username,user.getUsername()) && Objects.equals(pws,oldpassword)){
+                ChangeMessage.setText("Parola a fost schimbata !");
+                return;
+            }
+        }
+        ChangeMessage.setText("Date incorecte!");
+    }
+
+
+    private void persistUsers() {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.writerWithDefaultPrettyPrinter().writeValue(USERS_PATH.toFile(), users);
+        } catch (IOException e) {
+            throw new CouldNotWriteUserException();
+        }
+    }
 }
