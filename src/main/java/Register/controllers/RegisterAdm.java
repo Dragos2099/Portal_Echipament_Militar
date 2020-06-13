1bdca027d615c310623fa9aab81909f795248082
Dragos2099 <dragos.tudosoiu@student.upt.ro>
2020-06-14 00:30:45 +0300

    PEM-109 Fereastra inregistrare

---
+++ src/main/java/Register/controllers/RegisterAdm.java
@@ -0,0 +1,11 @@
+package Register.controllers;
+
+import javafx.event.ActionEvent;
+
+public class RegisterAdm {
+    public void handleRegisterAction(ActionEvent actionEvent) {
+    }
+
+    public void back(ActionEvent actionEvent) {
+    }
+}
