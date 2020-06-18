b247d1e9f06930ce5d01f696021dc77d116286a9
jeck00119 <jeck00119@gmail.com>
2020-06-19 02:52:16 +0300

    PEM-53 Fereastra creare oferta

---
+++ src/main/java/Oferta/controllers/Offer.java
@@ -0,0 +1,27 @@
+package Oferta.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class Offer {
+
+    @FXML
+    public TextField Bucati;
+    @FXML
+    public TextField Pret;
+    @FXML
+    public TextField Echipament;
+    @FXML
+    public DatePicker Date;
+    @FXML
+    public Text Mesaj;
+
+    public void Creare(ActionEvent actionEvent) {
+    }
+
+    public void Anulare(ActionEvent actionEvent) {
+    }
+}
