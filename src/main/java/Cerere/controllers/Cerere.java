bca89acf84ca65755c5c27c21dc99e35a3858eda
Dragos2099 <dragos.tudosoiu@student.upt.ro>
2020-06-18 20:35:05 +0300

    PEM-84  Text ChoiceBox Urgent fereastra cerere

--- src/main/java/Cerere/controllers/Cerere.java
+++ src/main/java/Cerere/controllers/Cerere.java
@@ -1,4 +1,14 @@
 package Cerere.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
 public class Cerere {
+    @FXML
+    public TextField Echipament;
+    @FXML
+    public TextField Bucati;
+    @FXML
+    public ChoiceBox Urgent;
 }
