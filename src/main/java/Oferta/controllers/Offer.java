d7a790b3a9e2b6fbdf392e7108d120acfc975639
jeck00119 <jeck00119@gmail.com>
2020-06-19 03:06:26 +0300

    PEM-122 Buton "Anulare"

--- src/main/java/Oferta/controllers/Offer.java
+++ src/main/java/Oferta/controllers/Offer.java
@@ -3,6 +3,7 @@ package Oferta.controllers;
 import Oferta.services.OfertaService;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
@@ -31,5 +32,6 @@ public class Offer {
     }
 
     public void Anulare(ActionEvent actionEvent) {
+        ((Node)(actionEvent.getSource())).getScene().getWindow().hide();
     }
 }
